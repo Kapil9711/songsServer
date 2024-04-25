ba07e51e3f0a8d16f07b4c509be9be26fbe60bdc
kapil <kapil.personal9711@gmail.com>
2024-04-25 21:58:14 +0530

    added username

--- model/favorite-model.js
+++ model/favorite-model.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose";
 
 const FavoriteSchema = mongoose.Schema({
   songId: String,
+  username: String,
 });
 export default mongoose.model("FavoriteSongsModel", FavoriteSchema);
